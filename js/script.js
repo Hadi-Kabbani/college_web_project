e9322c1ea8ba71fefafae2ab15017287c55d56f4
Hadi-Kabbani <hadikabbani76917223@gmail.com>
2024-05-10 00:59:58 +0300

    dawda

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var countDownDate = new Date("April 30, 2025 00:00:00").getTime();
+var x = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("days").innerHTML = formatTime(days);
+  document.getElementById("hours").innerHTML = formatTime(hours);
+  document.getElementById("minutes").innerHTML = formatTime(minutes);
+  document.getElementById("seconds").innerHTML = formatTime(seconds);
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}
+
+function formatTime(time) {
+  return time < 10 ? "0" + time : time;
+}
+
+// Function to update the countdown with a new date
+function updateCountdownDate(newDate) {
+  countDownDate = new Date(newDate).getTime();
+}
+
+// Example usage to change the countdown date to May 15, 2024
+updateCountdownDate("May 15, 2025 00:00:00");
